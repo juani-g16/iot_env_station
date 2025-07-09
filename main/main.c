a9888369fd634e3104cf079a9c9a885239361793
Juan Giorgetti <juan.giorgetti@itk-engineering.com>
2025-07-09 20:11:06 +0200

    Added new queue and task skeleton for mqtt processing

--- main/main.c
+++ main/main.c
@@ -38,12 +38,15 @@ static const char *TAG = "iot_env_station";
 esp_err_t set_timer(void);
 esp_err_t dht_init(void);
 esp_err_t create_tasks(void);
+
 void measure_temp_hum(TimerHandle_t timer);
 void task_show_data_oled(void *args);
+void task_send_data_mqtt(void *args);
 
 void app_main(void)
 {
     displayQueue = xQueueCreate(MAX_Q_SIZE, sizeof(dht_data_t));
+    mqttQueue = xQueueCreate(MAX_Q_SIZE, sizeof(dht_data_t));
     ESP_ERROR_CHECK(set_timer());
     ESP_ERROR_CHECK(dht_init());
     ESP_ERROR_CHECK(create_tasks());
@@ -99,14 +102,28 @@ esp_err_t dht_init(void)
 esp_err_t create_tasks(void)
 {
     static uint8_t ucParameterToPass;
-    TaskHandle_t handle = NULL;
+    TaskHandle_t displayHandle = NULL;
+    TaskHandle_t mqttHandle = NULL;
 
-    xTaskCreate(task_show_data_oled,
-                "Show read data on oled display",
-                STACK_SIZE,
-                &ucParameterToPass,
-                1,
-                &handle);
+    if (xTaskCreate(task_show_data_oled,
+                    "Show read data on oled display",
+                    STACK_SIZE,
+                    &ucParameterToPass,
+                    1,
+                    &displayHandle) != pdPASS)
+    {
+        return ESP_FAIL;
+    }
+
+    if (xTaskCreate(task_send_data_mqtt,
+                    "Send data to MQTT broker",
+                    STACK_SIZE,
+                    &ucParameterToPass,
+                    2,
+                    &mqttHandle) != pdPASS)
+    {
+        return ESP_FAIL;
+    }
 
     return ESP_OK;
 }
@@ -139,10 +156,9 @@ void task_show_data_oled(void *args)
     u8g2_ClearBuffer(&u8g2);
     /* End of setup*/
 
-    
     while (true)
     {
-        if (xQueueReceive(displayQueue,&recData,pdMS_TO_TICKS(MEASURE_INTERVAL)))
+        if (xQueueReceive(displayQueue, &recData, pdMS_TO_TICKS(MEASURE_INTERVAL)))
         {
             char tmp[20], hum[20];
             sprintf(tmp, "Temp: %.2fC", recData.temperature);
@@ -160,7 +176,24 @@ void task_show_data_oled(void *args)
         {
             ESP_LOGE(TAG, "Error receiving data or no data in buffer");
         }
-        
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+void task_send_data_mqtt(void *args)
+{
+    dht_data_t recData = {0};
+    while (true)
+    {
+        if (xQueueReceive(mqttQueue, &recData, pdMS_TO_TICKS(MEASURE_INTERVAL)))
+        {
+            /*code*/
+        }
+        else
+        {
+            ESP_LOGE(TAG, "Error receiving data or no data in buffer");
+        }
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
@@ -172,10 +205,14 @@ void measure_temp_hum(TimerHandle_t timer)
     res = dht_read_float_data(sensor_type, gpio_num, &dhtData.humidity, &dhtData.temperature);
     if (res == ESP_OK)
     {
-        ESP_LOGI(TAG,"Sending data: Temp: %.2fC - Humidity: %.2f%%",dhtData.temperature,dhtData.humidity);
+        ESP_LOGI(TAG, "Sending data: Temp: %.2fC - Humidity: %.2f%%", dhtData.temperature, dhtData.humidity);
         if (xQueueSend(displayQueue, &dhtData, pdMS_TO_TICKS(100)) != pdPASS)
         {
             ESP_LOGE(TAG, "Error sending data to display queue");
+        }
+        if (xQueueSend(mqttQueue, &dhtData, pdMS_TO_TICKS(100)) != pdPASS)
+        {
+            ESP_LOGE(TAG, "Error sending data to MQTT queue");
         }
     }
     else
